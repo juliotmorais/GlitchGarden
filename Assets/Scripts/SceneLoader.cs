0013be1d94ea4b1d3df7428e94d8075e0732f9d7
juliotmorais <juliotmorais@gmail.com>
2020-08-09 02:08:07 +0900

    Created Sceneloader, connected splash and start screens, imported assets for later use

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            LoadStartScreenFromSplashScreen();
+        }
+
+    }
+    public void LoadSplashScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void LoadStartScreen()
+    {
+        
+        SceneManager.LoadScene(1);
+    }
+    public void LoadStartScreenFromSplashScreen()
+    {
+        StartCoroutine(LoadStartScreenWithDelay());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadStartScreenWithDelay()
+    {
+
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(1);
+    }
+}
