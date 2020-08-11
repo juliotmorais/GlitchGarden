a1473285de67731b9af44b4d963a226062956bac
Julio <juliotmorais@gmail.com>
2020-08-11 16:22:18 +0900

    Created multiple spawners for the lizard

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    bool spawn = true;
+    [SerializeField] float minSpawnDelay=1f;
+    [SerializeField] float maxSpawnDelay=5f;
+    [SerializeField] Attacker attackerPrefab;
+
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+}
