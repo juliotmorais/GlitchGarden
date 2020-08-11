1d1e8825c82a92c1121a5d02781a9a142b7748d4
Julio <juliotmorais@gmail.com>
2020-08-11 13:46:36 +0900

    enabled left movement for lizard with attacker script

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
+
+    [SerializeField] [Range(0f,5f)] float WalkSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class Attacker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.left * WalkSpeed * Time.deltaTime);
     }
 }
