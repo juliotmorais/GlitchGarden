b424d141593583bc97a69e64fd815599585379dd
Julio <juliotmorais@gmail.com>
2020-08-14 16:32:44 +0900

    vfx particles on enemy death

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,17 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathEffect;
     public void DealDamage(float damage)
     {
         health -= damage;
         if (health <= 0)
         {
+            TriggerDeathEffects();
             Destroy(gameObject);
         }
     }
 
+    private void TriggerDeathEffects()
+    {
+        if (!deathEffect)
+        {
+            return;
+        }
+        else
+        {
+            GameObject deathFX= Instantiate(deathEffect, transform.position, transform.rotation);
+            Destroy(deathFX, 1f);
+        }
+    }
 }
