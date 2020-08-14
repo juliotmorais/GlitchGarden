5b2c12a26f5e4b8d1b0fdaba1f45aceb885521a4
Julio <juliotmorais@gmail.com>
2020-08-14 18:47:45 +0900

    cacti now spawn on click within defender spawn area

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+    private void OnMouseDown() { 
+        SpawnDefender(GetSquareClicked());
+    }
+
+    private void SpawnDefender(Vector2 coordinates)
+    {
+        GameObject newDefender = Instantiate(defender, coordinates, UnityEngine.Quaternion.identity) as GameObject;
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+
+    }
+}
