02af5f0df37e2330e3e45bfa9c08c9f326c8fdd3
juliotmorais <juliotmorais@gmail.com>
2020-08-14 09:12:58 +0900

    added colliders to lizard and zucchini, started health system

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -13,4 +13,12 @@ public class Projectile : MonoBehaviour
     {
         transform.Translate(Vector2.right*speed*Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        Debug.Log("I hit " + otherCollider.name);
+        DealDamage();
+        Destroy(gameObject);
+        
+    }
 }
