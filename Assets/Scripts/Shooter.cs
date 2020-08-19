a6858d622d80c96f2a7e68de92f6b1a24d1353d5
Julio <juliotmorais@gmail.com>
2020-08-19 12:02:12 +0900

    cactus now knows whether or not to shoot

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,13 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
+    EnemySpawner myLaneSpawner;
 
+    void Start()
+    {
+        SetLaneSpawner();
+    }
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("shoot it!");
+        }
+        else
+        {
+            Debug.Log("conserve fire");
+        }
+    }
     public void Fire(float damange)
     {
         Instantiate(projectile, gun.transform.position, Quaternion.identity);
     }
+
+    private void SetLaneSpawner()
+    {
+        EnemySpawner[] spawners = FindObjectsOfType<EnemySpawner>();
+        foreach (EnemySpawner spawn in spawners)
+        {
+            bool IsCloseEnough = (Math.Abs(spawn.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawn;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
 }
