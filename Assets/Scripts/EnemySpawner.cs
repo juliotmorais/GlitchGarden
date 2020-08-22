3ccad6876a3d56d15c7cb6c356ca71ab5b63287c
juliotmorais <juliotmorais@gmail.com>
2020-08-23 00:32:06 +0900

    created random enemy spawn in spawners

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,7 +8,7 @@ public class EnemySpawner : MonoBehaviour
     bool spawn = true;
     [SerializeField] float minSpawnDelay=1f;
     [SerializeField] float maxSpawnDelay=5f;
-    [SerializeField] Attacker attackerPrefab;
+    [SerializeField] Attacker[] attackerPrefabArray;
 
 
     // Start is called before the first frame update
@@ -31,7 +31,13 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnAttacker()
     {
-        Attacker newAttacker = Instantiate(attackerPrefab, transform.position, transform.rotation) as Attacker;
+        var attackerIndex = UnityEngine.Random.Range(0, attackerPrefabArray.Length);
+        Spawn(attackerPrefabArray[attackerIndex]);
+    }
+
+    private void Spawn(Attacker myAttacker)
+    {
+        Attacker newAttacker = Instantiate(myAttacker, transform.position, transform.rotation) as Attacker;
 
         newAttacker.transform.parent = transform;
 
